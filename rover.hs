f1a3c9ad5baba8651f52be3a64e3125a3f01239c
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-08-06 11:17:24 +0200

    feat(project): initial commit

---
+++ rover.hs
@@ -0,0 +1,43 @@
+data Instruction = F | B | L | R
+data Direction = N | S | E | W
+    deriving Eq
+data Position = Position Integer Integer
+    deriving Eq
+
+data WorldState = WorldState { roverPosition :: Position, roverDirection :: Direction, width :: Integer, height :: Integer, obstacles :: [Position]  }
+    deriving Eq
+
+step :: WorldState -> Instruction -> WorldState
+step (WorldState (Position roverX roverY) roverDirection width height obstacles) instruction = case instruction of
+    F -> case roverDirection of
+        N -> WorldState (Position roverX (roverY + 1)) roverDirection width height obstacles
+        S -> WorldState (Position roverX (roverY - 1)) roverDirection width height obstacles
+        E -> WorldState (Position (roverX + 1) roverY) roverDirection width height obstacles
+        W -> WorldState (Position (roverX - 1) roverY) roverDirection width height obstacles
+    B -> case roverDirection of
+        N -> WorldState (Position roverX (roverY - 1)) roverDirection width height obstacles
+        S -> WorldState (Position roverX (roverY + 1)) roverDirection width height obstacles
+        E -> WorldState (Position (roverX - 1) roverY) roverDirection width height obstacles
+        W -> WorldState (Position (roverX + 1) roverY) roverDirection width height obstacles
+    L -> case roverDirection of
+        N -> WorldState (Position roverX roverY) W width height obstacles
+        S -> WorldState (Position roverX roverY) E width height obstacles
+        E -> WorldState (Position roverX roverY) N width height obstacles
+        W -> WorldState (Position roverX roverY) S width height obstacles
+    R -> case roverDirection of
+        N -> WorldState (Position roverX roverY) E width height obstacles
+        S -> WorldState (Position roverX roverY) W width height obstacles
+        E -> WorldState (Position roverX roverY) S width height obstacles
+        W -> WorldState (Position roverX roverY) N width height obstacles
+
+initialPosition = Position 10 10
+initialState = WorldState initialPosition N 50 50 []
+
+finalPosition = Position 10 11
+finalState = WorldState finalPosition N 50 50 []
+
+main :: IO ()
+main = let
+        test1 = (step initialState F) == finalState
+       in
+        if test1 == True then print "yay" else print "boo"
