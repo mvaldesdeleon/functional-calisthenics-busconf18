02f1a87569ea506ac552362e8350036e38e89939
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-08-07 00:59:09 +0200

    feat(project): refactor moving logic, add looping over at edges

--- rover.hs
+++ rover.hs
@@ -26,18 +26,36 @@ turnRight worldState@WorldState { roverDirection } = worldState { roverDirection
 turnLeft :: WorldState -> WorldState
 turnLeft worldState@WorldState { roverDirection } = worldState { roverDirection = left roverDirection }
 
+advance :: Integer -> Integer -> Integer
+advance max value = if value == max - 1 then 0 else value + 1
+
+retreat :: Integer -> Integer -> Integer
+retreat max value = if value == 0 then max else value - 1
+
+goUp :: WorldState -> WorldState
+goUp worldState@WorldState { roverPosition = Position roverX roverY, height } = worldState { roverPosition = Position roverX (advance height roverY) }
+
+goDown :: WorldState -> WorldState
+goDown worldState@WorldState { roverPosition = Position roverX roverY, height } = worldState { roverPosition = Position roverX (retreat height roverY) }
+
+goRight :: WorldState -> WorldState
+goRight worldState@WorldState { roverPosition = Position roverX roverY, width } = worldState { roverPosition = Position (advance width roverX) roverY }
+
+goLeft :: WorldState -> WorldState
+goLeft worldState@WorldState { roverPosition = Position roverX roverY, width } = worldState { roverPosition = Position (retreat width roverX) roverY }
+
 step :: WorldState -> Instruction -> WorldState
 step worldState@(WorldState (Position roverX roverY) roverDirection _ _ _) instruction = case instruction of
     F -> case roverDirection of
-        N -> worldState { roverPosition = (Position roverX (roverY + 1)) }
-        S -> worldState { roverPosition = (Position roverX (roverY - 1)) }
-        E -> worldState { roverPosition = (Position (roverX + 1) roverY) }
-        W -> worldState { roverPosition = (Position (roverX - 1) roverY) }
+        N -> goUp worldState
+        S -> goDown worldState
+        E -> goRight worldState
+        W -> goLeft worldState
     B -> case roverDirection of
-        N -> worldState { roverPosition = (Position roverX (roverY - 1)) }
-        S -> worldState { roverPosition = (Position roverX (roverY + 1)) }
-        E -> worldState { roverPosition = (Position (roverX - 1) roverY) }
-        W -> worldState { roverPosition = (Position (roverX + 1) roverY) }
+        N -> goDown worldState
+        S -> goUp worldState
+        E -> goLeft worldState
+        W -> goRight worldState
     L -> turnLeft worldState
     R -> turnRight worldState
 
